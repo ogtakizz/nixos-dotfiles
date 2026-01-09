5ad34750e957dfad1127bf8a2e85498e4d137d19
ogtakizz <91156885+ogtakizz@users.noreply.github.com>
2026-01-09 12:59:10 -0300

    changed neovim from package to module

--- nixos/home.nix
+++ nixos/home.nix
@@ -1,60 +1,15 @@
 { config, pkgs, ... }:
 
 {
+  imports = [
+    ./modules/zsh.nix
+  ];  
+
 
   nixpkgs.config.allowUnfree = true;
 
   home.username = "garcia";
   home.homeDirectory = "/home/garcia";
-
-  programs.zsh = {
-    	enable = true;
-    	autosuggestion.enable = true;
-    	syntaxHighlighting.enable = true;
-   	oh-my-zsh = {
-    		enable = true;
-		plugins = [
-		  "git"
-		  "sudo"
-		  "colored-man-pages"
-		  "extract"
-		];
-		 theme = "";	 
-    	};
-
-	plugins = [
-	{
-	   name = "zsh-async";
-	   src = pkgs.fetchFromGitHub {
-	     owner = "mafredri";
-	     repo = "zsh-async";
-	     rev = "v1.8.6";
-	     sha256 = "sha256-Js/9vGGAEqcPmQSsumzLfkfwljaFWHJ9sMWOgWDi0NI=";
-	     };
-	}
-	{
-	   name = "Pure";
-	   src = pkgs.fetchFromGitHub {
-		owner = "sindresorhus";
-		repo = "pure";
-		rev = "v1.26.0";
-		sha256 = "sha256-AZSxP2g6BWoxyiSQH7yzbbbfGcwD8jgnXPPfcYwJUL0=";
-	   };
-	}
-      ];
-
-        initContent = "
-	   autoload -U promptinit; promptinit
-	   prompt pure	
-	";   
-
-        shellAliases = {
-      		nixedithome = "nvim ~/mysystem/nixos/home.nix";
-      		nixrebuild = "cd ~/mysystem && sudo nixos-rebuild switch --flake .#myNixos";
-      		nixedit = "nvim ~/mysystem/nixos/configuration.nix";
-    	};
-   };
-
 
   home.stateVersion = "25.05"; 
 
